e31d30b5e0352e2b3064ec1f2fccd58c866ff44e
Michał O <solairen@solairen.tech>
2023-05-23 14:06:13 +0200

    Refactor (#33)

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -2,33 +2,45 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 
-namespace test;
-
-[TestClass]
-public class UnitTest1
+namespace Test
 {
-    int _lenght;
-    string _char = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
-    string _randomString = String.Empty;
-
-    string _char_with_special = String.Empty;
-    string _special_char = "!@#$%^&*";
-    Random _random = new Random();
-    
-    [TestMethod]
-    public void Test1()
+    [TestClass]
+    public class PasswordGeneratorTests
     {
-        _lenght = 99;
-        _randomString = new string(Enumerable.Repeat(_char, _lenght).Select(s => s[_random.Next(s.Length)]).ToArray());
-        Assert.AreEqual(99, _randomString.Length);
-    }
+        private const string Characters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+        private const string SpecialCharacters = "!@#$%^&*";
+        private readonly Random random = new Random();
 
-    [TestMethod]
-    public void Test2()
-    {
-        _lenght = 6;
-        _char_with_special = _char + _special_char;
-        _randomString = new string(Enumerable.Repeat(_char_with_special, _lenght).Select(s => s[_random.Next(s.Length)]).ToArray());
-        Assert.AreEqual(6, _randomString.Length);
+        [TestMethod]
+        public void GeneratePassword_Length99_ReturnsPasswordWithLength99()
+        {
+            int length = 99;
+            string password = GeneratePassword(length, includeSpecialChar: false);
+            Assert.AreEqual(length, password.Length);
+        }
+
+        [TestMethod]
+        public void GeneratePassword_Length6WithSpecialChar_ReturnsPasswordWithLength6AndSpecialChar()
+        {
+            int length = 6;
+            string password = GeneratePassword(length, includeSpecialChar: true);
+            Assert.AreEqual(length, password.Length);
+            Assert.IsTrue(password.Any(c => SpecialCharacters.Contains(c)));
+        }
+
+        private string GeneratePassword(int length, bool includeSpecialChar)
+        {
+            string availableCharacters = Characters;
+
+            if (includeSpecialChar)
+            {
+                availableCharacters += SpecialCharacters;
+            }
+
+            string password = new string(Enumerable.Repeat(availableCharacters, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+
+            return password;
+        }
     }
 }
