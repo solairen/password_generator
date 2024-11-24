029f91d44791fd0b8986aa27b883067a5c7a1501
solairen <michal.oleszek@outlook.com>
2024-11-24 17:07:14 +0000

    Fixed Unit TEst

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 using System.Security.Cryptography;
+
 namespace Test
 {
   [TestClass]
@@ -9,23 +10,24 @@ namespace Test
   {
     private const string Characters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
     private const string SpecialCharacters = "!@#$%^&*";
-    private readonly RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+    private readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
 
-    [TestMethod]
-    public void GeneratePassword_Length99_ReturnsPasswordWithLength99()
+    [DataTestMethod]
+    [DataRow(99, false)]
+    [DataRow(6, true)]
+    public void GeneratePassword_Test(int length, bool includeSpecialChar)
     {
-      int length = 99;
-      string password = GeneratePassword(length, includeSpecialChar: false);
+      string password = GeneratePassword(length, includeSpecialChar);
       Assert.AreEqual(length, password.Length);
-    }
 
-    [TestMethod]
-    public void GeneratePassword_Length6WithSpecialChar_ReturnsPasswordWithLength6AndSpecialChar()
-    {
-      int length = 6;
-      string password = GeneratePassword(length, includeSpecialChar: true);
-      Assert.AreEqual(length, password.Length);
-      Assert.IsTrue(password.Any(c => SpecialCharacters.Contains(c)));
+      if (includeSpecialChar)
+      {
+        Assert.IsTrue(password.Any(c => SpecialCharacters.Contains(c)), "Password does not contain any special characters.");
+      }
+      else
+      {
+        Assert.IsFalse(password.Any(c => SpecialCharacters.Contains(c)), "Password contains special characters.");
+      }
     }
 
     private string GeneratePassword(int length, bool includeSpecialChar)
